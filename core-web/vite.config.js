1796a9e3791e0a3a6c9b3ebed4e30edb1bcec3cc
Ralph Barioga <bariogahot@gmail.com>
2026-04-14 11:36:35 +0800

    fix: clean main.js and update config alias

--- core-web/vite.config.js
+++ core-web/vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -7,8 +8,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      // We found index.mjs in your ls output, so use it here:
-      'vue-toastification': fileURLToPath(new URL('./node_modules/vue-toastification/dist/index.mjs', import.meta.url))
+      // Use a relative path that works on both Mac and Render
+      'vue-toastification': 'vue-toastification/dist/index.mjs'
     }
   }
 })
