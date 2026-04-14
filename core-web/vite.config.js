21f09e56c63e72c855854ea236024a8b6d4755f7
Ralph Barioga <bariogahot@gmail.com>
2026-04-14 11:29:59 +0800

    fix: clean main.js and update config alias

--- core-web/vite.config.js
+++ core-web/vite.config.js
@@ -2,12 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// vite.config.js
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue-toastification': 'vue-toastification/dist/index.mjs' 
+      // Change .mjs to .js here:
+      'vue-toastification': 'vue-toastification/dist/index.js'
     }
   }
 })
