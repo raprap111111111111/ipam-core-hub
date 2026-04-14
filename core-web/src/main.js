ea9ebc2121673dcc1173bcfb4640dddfa35a0475
Ralph Barioga <bariogahot@gmail.com>
2026-04-14 11:12:58 +0800

    Fix: Force uppercase D for Dashboard.vue

--- core-web/src/main.js
+++ core-web/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./layouts/Main.css";
-import Toast from "vue-toastification";
+import Toast from "vue-toastification/dist/index.mjs"; // Point exactly to the ESM file
 import "vue-toastification/dist/index.css";
 
 const app = createApp(App);
